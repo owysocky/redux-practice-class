b61847a87c74ea6edb8b10c744c4dfb771ec06ad
Olha Wysocky and Pavel Zanchuk <olha+pavel@>
2019-05-06 11:45:03 -0700

    create multiple redux

--- scripts2.js
+++ scripts2.js
@@ -6,14 +6,15 @@ const initialState = {
     arr: [1, 2, 3, 4, 5]
   },
   they: {
-    name: "Pavel and Olya",
+    name: "Liza and Colter",
     clicks: 0,
-    arr: [1, 2, 3, 4, 5]
+    arr: [6, 7, 8, 9]
   }
 };
 
 //REDUSER
-const reducer = (state = initialState, action) => {
+
+const reducerUs = (state = initialState.us, action) => {
   var newState;
   switch (action.type) {
     case "ADD":
@@ -36,48 +37,90 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-// REDUX STORE
-const { createStore } = Redux;
-const store = createStore(reducer);
-console.log(store.getState());
+const reducerThey = (state = initialState.they, action) => {
+  var newState;
+  switch (action.type) {
+    case "ADD_THEY":
+      let newClicks = state.clicks + 1;
+      newState = {
+        name: state.name,
+        clicks: newClicks,
+        arr: state.arr
+      };
+      return newState;
+    case "RESET_TO_ZERO_THEY":
+      newState = {
+        name: state.name,
+        clicks: 0,
+        arr: state.arr
+      };
+      return newState;
+    default:
+      return state;
+  }
+};
 
-//TESTS
-const { expect } = window;
-
-expect(reducer(initialState, { type: null })).toEqual(initialState);
-expect(reducer(initialState, { type: "ADD" })).toEqual({
-  name: "Pavel and Olya",
-  clicks: 1,
-  arr: [1, 2, 3, 4, 5]
+const rootReducer = this.Redux.combineReducers({
+  us: reducerUs,
+  they: reducerThey
 });
 
+// REDUX STORE
+const { createStore } = Redux;
+const store = createStore(rootReducer);
+console.log(store.getState());
+
 // RENDERING STATE IN DOM
-const renderNumber = () => {
-  const numberDisplay = document.getElementById("number");
+const renderNumberUs = () => {
+  const numberDisplay = document.getElementById("numberUs");
   while (numberDisplay.firstChild) {
     numberDisplay.removeChild(numberDisplay.firstChild);
   }
 
-  const number = store.getState().clicks;
+  const number = store.getState().us.clicks;
 
   const renderLine = document.createTextNode(number);
-  document.getElementById("number").appendChild(renderLine);
+  document.getElementById("numberUs").appendChild(renderLine);
+};
+
+const renderNumberThey = () => {
+  const numberDisplay = document.getElementById("numberThey");
+  while (numberDisplay.firstChild) {
+    numberDisplay.removeChild(numberDisplay.firstChild);
+  }
+
+  const number = store.getState().they.clicks;
+
+  const renderLine = document.createTextNode(number);
+  document.getElementById("numberThey").appendChild(renderLine);
 };
 
 window.onload = function() {
-  renderNumber();
+  renderNumberUs();
+  renderNumberThey();
 };
 
 // CLICK LISTENER
-const userClickAdd = () => {
+const userClickAddUs = () => {
   store.dispatch({ type: "ADD" });
   console.log(store.getState());
 };
 
-const userClickZero = () => {
+const userClickZeroUs = () => {
   store.dispatch({ type: "RESET_TO_ZERO" });
   console.log(store.getState());
 };
 
+const userClickAddThey = () => {
+  store.dispatch({ type: "ADD_THEY" });
+  console.log(store.getState());
+};
+
+const userClickZeroThey = () => {
+  store.dispatch({ type: "RESET_TO_ZERO_THEY" });
+  console.log(store.getState());
+};
+
 // SUBSCRIBE TO REDUX STORE
-store.subscribe(renderNumber);
+store.subscribe(renderNumberUs);
+store.subscribe(renderNumberThey);
