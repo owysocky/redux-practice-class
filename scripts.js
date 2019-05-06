3250dcb2ccc9db76aad59d15d9d1e279347552cc
Olha Wysocky and Pavel Zanchuk <olha+pavel@>
2019-05-06 10:58:25 -0700

    show in DOM

--- scripts.js
+++ scripts.js
@@ -7,15 +7,24 @@ const initialState = {
 
 //REDUSER
 const reducer = (state = initialState, action) =>{
+    var newState;
     switch(action.type){
         case 'ADD' :
         let newClicks = state.clicks+1;
-        let newState = {
+        newState = {
             name: state.name,
             clicks: newClicks,
             arr: state.arr
         }
         return newState; 
+        case 'RESET_TO_ZERO':
+        
+        newState = {
+            name: state.name,
+            clicks: 0,
+            arr: state.arr
+        } 
+        return newState;
     default:
         return state;
     }
@@ -34,3 +43,36 @@ expect(reducer(initialState, {type: null})).toEqual(initialState);
 expect(reducer(initialState, {type: 'ADD'})).toEqual({name: "Pavel and Olya",
 clicks: 1,
 arr: [1,2,3,4,5]});
+
+// RENDERING STATE IN DOM
+const renderNumber = () =>{
+    const numberDisplay = document.getElementById('number');
+    while (numberDisplay.firstChild){
+        numberDisplay.removeChild(numberDisplay.firstChild);
+    }
+
+    const number = store.getState().clicks;
+
+    const renderLine= document.createTextNode(number);
+    document.getElementById('number').appendChild(renderLine);
+}
+
+window.onload = function() {
+    renderNumber();
+  }
+
+  // CLICK LISTENER
+const userClickAdd = () =>{
+    store.dispatch({type: 'ADD'});
+    console.log(store.getState());
+    
+}
+
+const userClickZero = () =>{
+    store.dispatch({type: 'RESET_TO_ZERO'});
+    console.log(store.getState());
+    
+}
+
+// SUBSCRIBE TO REDUX STORE
+store.subscribe(renderNumber);
